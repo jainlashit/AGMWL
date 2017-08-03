299bd346746782b071e62594691569025580c1d8
ljmanso <luis.manso@gmail.com>
2017-08-03 17:14:54 +0200

    removed build dir

--- build/agmmission/src/CMakeFiles/agmmission_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "agmmission_automoc.cpp"
-  "CMakeFiles/agmmission_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/agmmission_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/tools/agminner/src/CMakeFiles/agminner_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "agminner_automoc.cpp"
-  "CMakeFiles/agminner_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/agminner_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
